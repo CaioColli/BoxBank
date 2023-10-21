d829ce9a90ab5b27f5291d4ca05f153239d531e9
Caio Colli <caiocolli22@gmail.com>
2023-10-20 22:00:17 -0300

    Atualizações e modificações

---
+++ script/interacoes.js
@@ -0,0 +1,22 @@
+// Intereção do "Para você" do header
+
+const paraVoce = document.getElementById('mostrar_conteudo')
+const conteudoParaVoce = document.querySelector('.conteudo_paraVoce')
+
+paraVoce.addEventListener('mouseenter', () => {
+  conteudoParaVoce.style.height = '320px'
+})
+
+conteudoParaVoce.addEventListener('mousemove', () => {
+  conteudoParaVoce.style.height = '320px'
+})
+
+paraVoce.addEventListener('mouseleave', () => {
+  conteudoParaVoce.style.height = '0px'
+})
+
+conteudoParaVoce.addEventListener('mouseleave', () => {
+  conteudoParaVoce.style.height = '0px'
+})
+
+//
--- script/valida_cpf.js
+++ script/valida_cpf.js
@@ -1,4 +1,57 @@
-export default function validarCPF(cpf) {
+const formularioCPF = document.querySelector('[data-formularioCPF]')
+const inputCPF = document.querySelector('[data-inputCPF]')
+const btnEnviarCPF = document.querySelector('[data-inputEnviar]')
+
+const inputVazio = document.querySelector('[data-inputVazio]')
+const inputInvalido = document.querySelector('[data-inputInvalido]')
+
+inputCPF.addEventListener('input', () => {
+    let valorCPF = inputCPF.value
+
+    const cpfSemDigitos = valorCPF.replace(/\D/g, '')
+    inputCPF.value = cpfSemDigitos
+
+    let cpfValido = validarCPF(cpfSemDigitos)
+    let cpfInvalido = !cpfValido
+
+    // Para vizualizar se CPF é válido ou não
+    console.log(cpfValido)
+
+    valorCPF = cpfSemDigitos.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+    inputCPF.value = valorCPF
+
+    if (cpfValido) {
+        btnEnviarCPF.classList.remove('btn_submit')
+        btnEnviarCPF.classList.add('btn_submit_vermelho')
+    }
+
+    if (inputCPF.value.length > 11 && cpfInvalido) {
+        btnEnviarCPF.disabled = true
+        inputInvalido.style.display = 'block'
+    } else {
+        btnEnviarCPF.disabled = false
+    }
+
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 8) {
+            inputCPF.value = ''
+            btnEnviarCPF.classList.remove('btn_submit_vermelho')
+            btnEnviarCPF.classList.add('btn_submit')
+            inputInvalido.style.display = 'none'
+        }
+    })
+
+    if (inputCPF.validity.valid) {
+        inputVazio.style.display = 'none'
+    }
+})
+
+inputCPF.addEventListener('invalid', function (event) {
+    event.preventDefault()
+    inputVazio.style.display = 'block'
+})
+
+function validarCPF(cpf) {
     // Remove todos os caracteres que não sejam dígitos
     cpf = cpf.replace(/\D/g, '')
   
