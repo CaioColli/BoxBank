0910023c2210fa1a5c9cfc65c1fecbab5fd614f1
Caio Colli <110783734+CaioColli@users.noreply.github.com>
2023-10-22 11:42:22 -0300

    Atualizações
    Adicionei um celular que tem manipulações DOM, ainda não finalizado

--- script/interacoes.js
+++ script/interacoes.js
@@ -19,4 +19,11 @@ conteudoParaVoce.addEventListener('mouseleave', () => {
   conteudoParaVoce.style.height = '0px'
 })
 
-//
+// Funções celular
+
+const telaInicial = document.querySelector('#inicial')
+const botaoTelaInicial = document.querySelector('#btn_inicial')
+
+botaoTelaInicial.addEventListener('click', () => {
+  telaInicial.style.display = 'none'
+})
